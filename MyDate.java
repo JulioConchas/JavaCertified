6f286e58cc86f26d03f0b6b0d3cca852e65f8289
Julio Conchas <jconchas@ibm.com>
2024-07-03 23:13:00 -0600

    Lab01 Creating Classes, Bonus Lab: Add an initialization block

---
+++ MyDate.java
@@ -0,0 +1,34 @@
+package JavaCertified;
+/*
+ * 06/27/2024 Julio Conchas Lab01 Creating Classes
+ */
+public class MyDate 
+{
+    public int day;
+    public int year;
+    public int month;
+
+    {
+        this.day = 1;
+        this.month = 1;
+        this.year = 2000;
+    }
+
+    public MyDate(){}
+    public MyDate(int month, int day, int year)
+    {
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
+    public void setDate(int month, int day, int year)
+    {
+        this.month = month;
+        this.day = day;
+        this.year = year;
+    }
+    public String toString()
+    {
+        return month + "/" + day + "/" + year;
+    }
+}
