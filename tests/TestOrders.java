d1eb3ac8c04782573828089861455face27d8324
Julio Conchas <jconchas@ibm.com>
2024-07-31 23:58:55 -0600

    Lab 13 DateTime, lambda expression that work with LocalDate

--- tests/TestOrders.java
+++ tests/TestOrders.java
@@ -9,6 +9,8 @@ import JavaCertified.Good;
 import JavaCertified.Good.UnitOfMeasureType;
 import JavaCertified.Service;
 
+import java.time.LocalDate;
+
 public class TestOrders 
 {
     public static void main(String[] args)
@@ -20,6 +22,10 @@ public class TestOrders
         MyDate date2 = new MyDate(4,10,2008);
         Solid solid_balloon = new Solid("Acme Balloon", 1401, 15, UnitOfMeasureType.CUBIC_FEET, false, 10, 5, 5);
         Order balloons = new Order(date2, 1000.00, "Bugs Bunny", solid_balloon , 125);
+
+        MyDate hammerDate = new MyDate( 5, 17, 2016 );
+        Solid hammerType = new Solid("Acme Hammer", 281, 0.3, UnitOfMeasureType.CUBIC_METER, false, 100, 0.25, 0.3);
+        Order hammer = new Order(hammerDate, 10.00, "Wile E Coyote", hammerType, 10);
 
         // MyDate date3 = new MyDate(5,20,2008);
         // Order anotherAnvil = new Order(date3, 200, "Road Runner");
@@ -62,7 +68,18 @@ public class TestOrders
 
         System.out.println("The total bill for:  " + birdEradication + " is " + birdEradication.computeTotal());
 
-        Order.setRushable((orderDate, orderAmount) -> orderAmount > 1500);
+       // Order.setRushable((orderDate, orderAmount) -> orderAmount > 1500);
+       Order.setRushable( (orderDate, orderAmount) -> {
+        // Create a LocalDate object for now
+        LocalDate now = LocalDate.now();
+        // Create a LocalDate object for the order date.
+        LocalDate oDate = LocalDate.of(orderDate.getYear(), orderDate.getMonth(), orderDate.getDay());
+        // Add one month to the order date.
+        oDate = oDate.plusMonths(1);
+        // If the current date is after the order date + one month 
+        // It's rushable
+        return now.isAfter(oDate);
+       } );
 
         System.out.println("Anvil isPriorityOrde: " + anvil.isPriorityOrder());
         System.out.println("Balloons is PriorityOrder: " + balloons.isPriorityOrder());
